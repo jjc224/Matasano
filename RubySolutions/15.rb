e61496eba33bd6c6465752e74e2032e637895e42
jjc224 <jjc224@uowmail.edu.au>
2016-02-04 00:11:19 +1100

    #15 complete.

---
+++ RubySolutions/15.rb
@@ -0,0 +1,19 @@
+# Challenge 15: PKCS#7 padding validation
+require_relative 'matasano_lib/pkcs7'
+
+# Write a function that takes a plaintext, determines if it has valid PKCS#7 padding, and strips the padding off.
+# If you are writing in a language with exceptions, like Python or Ruby, make your function throw an exception on bad padding.
+def pkcs7_validate_and_strip(str)
+	raise 'Bad padding.' unless MatasanoLib::PKCS7.valid(str)
+	MatasanoLib::PKCS7.strip(str)
+end
+
+msg       = 'jaz0r'
+input     = MatasanoLib::PKCS7.pad(msg)
+blocksize = 16
+
+# p pkcs7_validate_and_strip(input)           # "jaz0r"
+# p pkcs7_validate_and_strip(input[0..-1])    # Bad padding. (RuntimeError)
+
+# input[-1] = input[-1].next
+# p pkcs7_validate_and_strip(input)           # Bad padding. (RuntimeError)
--- RubySolutions/matasano_lib/pkcs7.rb
+++ RubySolutions/matasano_lib/pkcs7.rb
@@ -8,18 +8,19 @@ module MatasanoLib
 				str << padding.chr * padding
 			end
 
+			def valid(str)
+				padding   = str[-1]
+				pad_start = str.length - padding.ord
+
+				return str[pad_start..-1] == padding * padding.ord
+			end
+
 			def strip(str, blocksize = @@blocksize)
 				padding   = str[-1]
 				pad_start = str.length - padding.ord
 
-				# Return the string if it does not end with a padding character.
-				# return str unless padding.between?(0.chr, @@blocksize.chr)
-
-				# Raise an exception if the anticipated padding does not conform to PKCS#7.
-				# raise 'Bad padding.' + " #{str[pad_start..-1].unpack('H*')}" if str[pad_start..-1] != padding * padding.ord
-
 				# My bad, man, didn't mean to fuck with your string. Here you go. Sorry for any inconvenience.
-				return str if str[pad_start..-1] != padding * padding.ord
+				return str unless valid(str)
 				str[0..pad_start - 1]
 			end
 		end
