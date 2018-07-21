d648b52364e8703691fa1566a30e94269f822202
jjc224 <jjc224@uowmail.edu.au>
2018-07-21 14:17:30 +1000

    MatasanoLib::AES_128{,_COMMON} TODO notes and minor touching up.

--- Ruby/matasano_lib/aes_128.rb
+++ Ruby/matasano_lib/aes_128.rb
@@ -9,9 +9,9 @@ module MatasanoLib
 
 			def encrypt(plaintext, key, mode, opts = {})
 				ciphertext = case mode
-					     when :ECB then AES_128_ECB.encrypt(plaintext, key, opts)
-					     when :CBC then AES_128_CBC.encrypt(plaintext, key, opts)
-					     when :CRT then AES_128_CRT.crypt(plaintext, key, opts)
+					         when :ECB then AES_128_ECB.encrypt(plaintext, key, opts)
+					         when :CBC then AES_128_CBC.encrypt(plaintext, key, opts)
+					         when :CRT then AES_128_CRT.crypt(plaintext, key, opts)
 					     end
 
 				ciphertext
@@ -19,9 +19,9 @@ module MatasanoLib
 
 			def decrypt(ciphertext, key, mode, opts = {})
 				plaintext = case mode
-					    when :ECB then AES_128_ECB.decrypt(ciphertext, key, opts)
-					    when :CBC then AES_128_CBC.decrypt(ciphertext, key, opts)
-					    when :CRT then AES_128_CRT.crypt(ciphertext, key, opts)
+					        when :ECB then AES_128_ECB.decrypt(ciphertext, key, opts)
+					        when :CBC then AES_128_CBC.decrypt(ciphertext, key, opts)
+					        when :CRT then AES_128_CRT.crypt(ciphertext, key, opts)
 					    end
 
 				plaintext
--- Ruby/matasano_lib/aes_128_common.rb
+++ Ruby/matasano_lib/aes_128_common.rb
@@ -2,36 +2,38 @@ require 'securerandom'
 
 module MatasanoLib
 	module AES_128_COMMON
-		@@blocksize = 16
+		class << self
+			@@blocksize = 16    # TODO: use this globally.
 
-		def random_key
-			SecureRandom.random_bytes
-		end
-
-		def detect_mode(ciphertext)    # Expecting hex formatted ciphertext.
-			blocks      = ciphertext.scan(/.{1,32}/)    # Split into 16-byte blocks; working with hex, so 32 characters.
-			blocks_dups = {}
-
-			# Iterate through the unique elements.
-			# Store the count of each duplicate element in a hash for output.
-			blocks.uniq.select do |block|
-				count = blocks.count(block)
-				blocks_dups[block] = count if count > 1
+			def random_key
+				SecureRandom.random_bytes
 			end
 
-			blocks_dups.empty? ? 'CBC' : 'ECB'
-		end
+			def detect_mode(ciphertext)    # Expecting hex formatted ciphertext.
+				blocks      = ciphertext.scan(/.{1,32}/)    # Split into 16-byte blocks; working with hex, so 32 characters.
+				blocks_dups = {}
 
-		def determine_blocksize(char = 'A')
-			input     = char
-			curr_size = yield(input).length
+				# Iterate through the unique elements.
+				# Store the count of each duplicate element in a hash for output.
+				blocks.uniq.select do |block|
+					count = blocks.count(block)
+					blocks_dups[block] = count if count > 1
+				end
 
-			loop do
-				input << char
-				break if yield(input).length > curr_size
+				blocks_dups.empty? ? 'CBC' : 'ECB'
 			end
 
-			yield(input).length - curr_size
+			def determine_blocksize(char = 'A')
+				input     = char
+				curr_size = yield(input).length
+
+				loop do
+					input << char
+					break if yield(input).length > curr_size
+				end
+
+				yield(input).length - curr_size
+			end
 		end
 	end
 end
