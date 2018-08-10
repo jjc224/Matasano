de58b3bd79c1282d30eae83dbce8a9c4fc29ee4e
jjc224 <youngphizo@gmail.com>
2018-08-11 01:39:07 +1000

    Retabbed #21 and #22.

--- Ruby/20.rb
+++ Ruby/20.rb
@@ -1,93 +1,130 @@
 # Challenge 20: break fixed-nonce CTR mode statistically.
 #
-# I intuitively took this statistical approach which you were meant to do in this  challenge in the previous instead (break via subsitutions).
+# I intuitively took this statistical approach which you were meant to do in this challenge in the previous instead (break via subsitutions).
 # I'll be reusing this code as my solution for the next one. I see no point in tediously rewriting a "suboptimal solution" as cryptopals put it.
 #
-# I'll tidy this up and write some notes. Will add output later.
-# 4 PM, no sleep: too tired.
+# Another bottom-up, dynamic programming approach to solving this puzzle. It simplified the process greatly.
 
 require_relative 'matasano_lib/monkey_patch'
 require_relative 'matasano_lib/aes_128'
 require_relative 'matasano_lib/xor'
 
 # Take your CTR encrypt/decrypt function and fix its nonce value to 0. Generate a random AES key.
-$AES_KEY = '4d79fe4bcacbd950db9b20bfc656259c'.unhex
+AES_KEY = '4d79fe4bcacbd950db9b20bfc656259c'.unhex
 
 # In successive encryptions (not in one big running CTR stream), encrypt each line of the base64 decodes of the following, producing multiple independent ciphertexts.
-$plaintexts = [
-               'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
-               'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
-               'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
-               'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
-               'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
-               'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
-               'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
-               'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
-               'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
-               'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
-               'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
-               'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
-               'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
-               'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
-               'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
-               'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
-               'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
-               'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
-               'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
-               'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
-               'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
-               'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
-               'U2hlIHJvZGUgdG8gaGFycmllcnM/',
-               'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
-               'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
-               'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
-               'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
-               'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
-               'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
-               'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
-               'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
-               'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
-               'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
-               'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
-               'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
-               'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
-               'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
-               'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
-               'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
-               'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
-             ]
+PLAINTEXTS = [
+  'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
+  'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
+  'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
+  'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
+  'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
+  'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+  'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
+  'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+  'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
+  'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
+  'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
+  'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
+  'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
+  'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
+  'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
+  'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
+  'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
+  'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
+  'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
+  'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
+  'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
+  'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
+  'U2hlIHJvZGUgdG8gaGFycmllcnM/',
+  'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
+  'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
+  'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
+  'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
+  'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
+  'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
+  'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
+  'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
+  'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
+  'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
+  'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
+  'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
+  'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
+  'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
+  'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
+  'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
+  'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
+]
 
-# We want to take every k'th character from the ciphertext C into its own respective column (it is traveresed as a column, but I store it as a row/array).
-# Each byte in the column is correspondently XOR'd with the most probable evil plaintext byte to produce the relevant keystream byte as K = C ^ P'. We know this because P = C ^ K, naturally.
-def decrypt_next_byte(ciphertexts, k)
-    column = ''
+# Transpose the ciphertext C into its own respective column (i.e. select every i'th character from every block of C).
+# Each byte in the column is correspondently XOR'd with the most probable evil plaintext byte to produce the relevant keystream byte K = C ^ P'. We know this because P = C ^ K, naturally.
+# The most probable K for (P', C) will be returned.
+def decrypt_next_byte(ciphertexts, i)
+  column = ciphertexts.map { |c| c[i] || '' }.join
 
-    ciphertexts.each do |c|
-        column << c[k].to_s
-    end
-
-    solution = MatasanoLib::XOR.brute(column.to_hex, "ETAOIN SHRDLU.,?;:")
-    solution[:key]
+  solution = MatasanoLib::XOR.brute(column, 'ETAOIN SHRDLU,.')
+  solution[:key]
 end
 
 def decrypt_keystream(ciphertexts)
-    keystream = ''
+  keystream = ''
 
-    ciphertexts.map.with_index do |c, i|
-        candidate_keys = []
+  ciphertexts.map.with_index do |c, i|
+    candidate_key = decrypt_next_byte(ciphertexts, i)     # Do '(c.max_by(&:size).size).times' if necessary (code seems to be highly robust and derives the same, correct key for every instance.
+    keystream << candidate_key unless candidate_key.nil?  # In which case, you will want to append the most common key in from array of candidate keys to keystream instead.
+  end
 
-        c.size.times do
-            candidate_keys << decrypt_next_byte(ciphertexts, i)
-        end
-
-        keystream << candidate_keys[0].to_s
-    end
-
-    keystream
+  keystream
 end
 
-ciphertexts = $plaintexts.map { |enc| MatasanoLib::AES_128.encrypt(enc.decode64, $AES_KEY, :mode => :CTR) }
-keystream   = decrypt_keystream(ciphertexts)
+def break_fixed_nonce_ctr
+  ciphertexts = PLAINTEXTS.map { |enc| MatasanoLib::AES_128.encrypt(enc.decode64, AES_KEY, :mode => :CTR) }
+  keystream   = decrypt_keystream(ciphertexts)
 
-ciphertexts.each { |enc| p MatasanoLib::XOR.crypt(enc, keystream).unhex }
+  ciphertexts.map { |enc| MatasanoLib::XOR.crypt(enc, keystream).unhex }
+end
 
+break_fixed_nonce_ctr.each { |p| p p }
+
+# Output
+# ---------------------------------------------------
+# "i have met them at close of day"
+# "coming with vivid faces"
+# "from counter or desk among grey"
+# "eighteenth-century houses."
+# "i have passed with a nod of the ,e\x06e"
+# "or polite meaningless words,"
+# "or have lingered awhile and said"
+# "polite meaningless words,"
+# "and thought before I had done"
+# "of a mocking tale or a gibe"
+# "to please a companion"
+# "around the fire at the club,"
+# "being certain that they and I"
+# "but lived where motley is worn:"
+# "all changed, changed utterly:"
+# "a terrible beauty is born."
+# "that woman's days were spent"
+# "in ignorant good will,"
+# "her nights in argument"
+# "until her voice grew shrill."
+# "what voice more sweet than hers"
+# "when young and beautiful,"
+# "she rode to harriers?"
+# "this man had kept a school"
+# "and rode our winged horse."
+# "this other his helper and friend"
+# "was coming into his force;"
+# "he might have won fame in the en ,"
+# "so sensitive his nature seemed,"
+# "so daring and sweet his thought."
+# "this other man I had dreamed"
+# "a drunken, vain-glorious lout."
+# "he had done most bitter wrong"
+# "to some who are near my heart,"
+# "yet I number him in the song;"
+# "he, too, has resigned his part"
+# "in the casual comedy;"
+# "he, too, has been changed in hisdt\x12s\x02A"
+# "transformed utterly:"
+# "a terrible beauty is born."
--- Ruby/21.rb
+++ Ruby/21.rb
@@ -11,48 +11,48 @@ $l = 18
 $mt = Array.new($n)
 $index = $n + 1
 
-LOWER_MASK = (1 << $r) - 1    # The binary number representation of 'r' 1's.
+LOWER_MASK = (1 << $r) - 1  # The binary number representation of 'r' 1's.
 UPPER_MASK = ((1 << $w) - 1) & ~LOWER_MASK
 
 # Initialize the generator with a seed.
 def seed_mt(seed)
-    $index = $n
-    $mt[0] = seed
-    f      = 1812433253    # The value for f for MT19937 (32-bit).
+  $index = $n
+  $mt[0] = seed
+  f      = 1812433253  # The value for f for MT19937 (32-bit).
 
-    # Loop over each element.
-    for i in 1..($n - 1)
-        $mt[i] = ((1 << $w) - 1) & (f * ($mt[i - 1] ^ ($mt[i - 1] >> ($w - 2))) + i)
-    end
+  # Loop over each element.
+  for i in 1..($n - 1)
+    $mt[i] = ((1 << $w) - 1) & (f * ($mt[i - 1] ^ ($mt[i - 1] >> ($w - 2))) + i)
+  end
 end
 
 # Extract a tempered value based on MT[index] calling twist() every n numbers
 def extract_number
-    if $index >= $n
-        raise "Generator was never seeded." if $index > $n    # Alternatively, seed with constant value; 5489 is used in reference C code.
-        twist()
-    end
+  if $index >= $n
+    raise "Generator was never seeded." if $index > $n  # Alternatively, seed with constant value; 5489 is used in reference C code.
+    twist()
+  end
 
-    y = $mt[$index]
-    y = y ^ ((y >> $u) & $d)
-    y = y ^ ((y << $s) & $b)
-    y = y ^ ((y << $t) & $c)
-    y = y ^ (y >> $l)
+  y = $mt[$index]
+  y = y ^ ((y >> $u) & $d)
+  y = y ^ ((y << $s) & $b)
+  y = y ^ ((y << $t) & $c)
+  y = y ^ (y >> $l)
 
-    $index += 1
+  $index += 1
 
-    ((1 << $w) - 1) & y
+  ((1 << $w) - 1) & y
 end
 
 def twist
-    for i in 0..($n - 1)
-        x      = ($mt[i] & UPPER_MASK) + ($mt[(i + 1) % $n] & LOWER_MASK)
-        xA     = x >> 1
-        xA    ^= $a if x & 1
-        $mt[i] = $mt[(i + $m) % $n] ^ xA
-    end
+  for i in 0..($n - 1)
+    x      = ($mt[i] & UPPER_MASK) + ($mt[(i + 1) % $n] & LOWER_MASK)
+    xA     = x >> 1
+    xA    ^= $a if x & 1
+    $mt[i] = $mt[(i + $m) % $n] ^ xA
+  end
 
-    $index = 0    # Not meant as return.
+  $index = 0  # Not meant as return.
 end
 
 seed_mt(0x1337)
