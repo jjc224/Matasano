41bd9fb918a097d6d747782d9b23b13e1e6c1614
jjc224 <youngphizo@gmail.com>
2018-08-11 01:37:39 +1000

    Retabbed and plucked a potential bug in which an array would be indexed with nil on failure.

--- Ruby/22.rb
+++ Ruby/22.rb
@@ -11,48 +11,48 @@ $l = 18
 $mt = Array.new($n)
 $index = $n + 1
 
-LOWER_MASK = (1 << $r) - 1    # The binary number representation of 'r' 1's.
+LOWER_MASK = (1 << $r) - 1  # The binary number representation of 'r' 1's.
 UPPER_MASK = ((1 << $w) - 1) & ~LOWER_MASK
 
 # Initialize the generator with a seed.
 def seed_mt(seed)
-    $index = $n
-    $mt[0] = seed
-    f      = 1812433253    # The value for f for MT19937 (32-bit).
+  $index = $n
+  $mt[0] = seed
+  f      = 1812433253  # The value for f for MT19937 (32-bit).
 
-    # Loop over each element.
-    for i in 1..($n - 1)
-        $mt[i] = ((1 << $w) - 1) & (f * ($mt[i - 1] ^ ($mt[i - 1] >> ($w - 2))) + i)
-    end
+  # Loop over each element.
+  for i in 1..($n - 1)
+    $mt[i] = ((1 << $w) - 1) & (f * ($mt[i - 1] ^ ($mt[i - 1] >> ($w - 2))) + i)
+  end
 end
 
 # Extract a tempered value based on MT[index] calling twist() every n numbers.
 def rand_mt
-    if $index >= $n
-        raise "Generator was never seeded." if $index > $n    # Alternatively, seed with constant value; 5489 is used in reference C code.
-        twist()
-    end
+  if $index >= $n
+    raise "Generator was never seeded." if $index > $n  # Alternatively, seed with constant value; 5489 is used in reference C code.
+    twist()
+  end
 
-    y = $mt[$index]
-    y = y ^ ((y >> $u) & $d)
-    y = y ^ ((y << $s) & $b)
-    y = y ^ ((y << $t) & $c)
-    y = y ^ (y >> $l)
+  y = $mt[$index]
+  y = y ^ ((y >> $u) & $d)
+  y = y ^ ((y << $s) & $b)
+  y = y ^ ((y << $t) & $c)
+  y = y ^ (y >> $l)
 
-    $index += 1
+  $index += 1
 
-    ((1 << $w) - 1) & y
+  ((1 << $w) - 1) & y
 end
 
 def twist
-    for i in 0..($n - 1)
-        x      = ($mt[i] & UPPER_MASK) + ($mt[(i + 1) % $n] & LOWER_MASK)
-        xA     = x >> 1
-        xA    ^= $a if x & 1
-        $mt[i] = $mt[(i + $m) % $n] ^ xA
-    end
+  for i in 0..($n - 1)
+    x      = ($mt[i] & UPPER_MASK) + ($mt[(i + 1) % $n] & LOWER_MASK)
+    xA     = x >> 1
+    xA    ^= $a if x & 1
+    $mt[i] = $mt[(i + $m) % $n] ^ xA
+  end
 
-    $index = 0    # Not meant as return.
+  $index = 0  # Not meant as return.
 end
 
 # Write a routine that performs the following operation:
@@ -68,27 +68,27 @@ end
 # An insecure implementation of MT19937.
 # Suppose it is a blackbox function (it's a greybox function ;~)).
 def blackbox_mt19937
-    sleep(rand(40..1_000))
-    seed_mt(Time.now.to_i)
-    sleep(rand(40..1_000))
+  sleep(rand(4..7))
+  seed_mt(Time.now.to_i)
+  sleep(rand(4..7))
 
-    rand_mt    # Already returns a 32-bit int masked by 0xffffffff ((1 << $w) - 1).
+  rand_mt  # Already returns a 32-bit int masked by 0xffffffff ((1 << $w) - 1).
 end
 
 def crack_blackbox_mt19937
-    # As we are seeding with the current time padded between two intervals of time, it is simply a matter of brute-forcing the seed between that interval (when it's short like in this case).
-    # We know that the seed is in the interval [t1, t2]. t2 - t1 also gives us an estimation of how long the function ran for.
-    t1 = Time.now.to_i
-    r  = blackbox_mt19937
-    t2 = Time.now.to_i
+  # As we are seeding with the current time padded between two intervals of time, it is simply a matter of brute-forcing the seed between that interval (when it's short like in this case).
+  # We know that the seed is in the interval [t1, t2]. t2 - t1 also gives us an estimation of how long the function ran for.
+  t1 = Time.now.to_i
+  r  = blackbox_mt19937
+  t2 = Time.now.to_i
 
-    interval = (t1..t2).to_a   # It doesn't hurt to check for t1 and t2. May as well make it an inclusive interval.
-    index    = interval.index { |seed| seed_mt(seed); r == rand_mt }
+  interval = (t1..t2).to_a  # It doesn't hurt to check for t1 and t2. May as well make it an inclusive interval.
+  index    = interval.index { |seed| seed_mt(seed); r == rand_mt }
 
-    interval[index]
+  interval[index] unless index.nil?
 end
 
-puts "Seed: #{crack_blackbox_mt19937.to_s}"
+puts "Seed: #{crack_blackbox_mt19937.to_s}"  # This would output 'Seed: nil' on failure.
 
 # Output:
 #------------------------------------ ----------------------------
