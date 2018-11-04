4ee01a0a3587e7214fdff03b93cd9e24a86a1d28
jjc224 <jjc224@uowmail.edu.au>
2018-11-05 10:32:42 +1100

    Updated tabs and superfluous map to each (loop).

--- Ruby/19.rb
+++ Ruby/19.rb
@@ -15,69 +15,71 @@ $AES_KEY = '4d79fe4bcacbd950db9b20bfc656259c'.unhex
 
 # In successive encryptions (not in one big running CTR stream), encrypt each line of the base64 decodes of the following, producing multiple independent ciphertexts.
 $plaintexts = [
-               'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
-               'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
-               'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
-               'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
-               'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
-               'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
-               'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
-               'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
-               'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
-               'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
-               'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
-               'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
-               'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
-               'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
-               'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
-               'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
-               'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
-               'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
-               'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
-               'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
-               'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
-               'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
-               'U2hlIHJvZGUgdG8gaGFycmllcnM/',
-               'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
-               'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
-               'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
-               'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
-               'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
-               'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
-               'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
-               'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
-               'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
-               'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
-               'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
-               'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
-               'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
-               'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
-               'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
-               'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
-               'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
-             ]
+  'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
+  'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
+  'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
+  'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
+  'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
+  'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+  'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
+  'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+  'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
+  'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
+  'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
+  'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
+  'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
+  'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
+  'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
+  'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
+  'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
+  'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
+  'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
+  'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
+  'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
+  'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
+  'U2hlIHJvZGUgdG8gaGFycmllcnM/',
+  'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
+  'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
+  'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
+  'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
+  'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
+  'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
+  'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
+  'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
+  'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
+  'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
+  'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
+  'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
+  'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
+  'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
+  'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
+  'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
+  'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
+]
 
 # We want to take every k'th character from the ciphertext C into its own respective column (it is traveresed as a column, but I store it as a row/array).
 # Each byte in the column is correspondently XOR'd with the most probable evil plaintext byte to produce the relevant keystream byte as K = C ^ P'. We know this because P = C ^ K, naturally.
 def decrypt_next_byte(ciphertexts, k)
-    column = ''
+  column = ''
 
-    ciphertexts.each { |c| column << c[k].to_s }
+  ciphertexts.each do
+    |c| column << c[k].to_s
+  end
 
-    solution = MatasanoLib::XOR.brute(column, "ETAOIN SHRDLU.,?;:")
-    solution[:key]
+  solution = MatasanoLib::XOR.brute(column, "ETAOIN SHRDLU.,?;:")
+  solution[:key]
 end
 
 def decrypt_keystream(ciphertexts)
-    keystream = ''
+  keystream = ''
 
-    ciphertexts.map.with_index do |c, i|
-        candidate_keys = []
-        c.size.times { candidate_keys << decrypt_next_byte(ciphertexts, i) }
-        keystream << candidate_keys[0].to_s
-    end
+  ciphertexts.each_with_index do |c, i|
+    candidate_keys = []
+    c.size.times { candidate_keys << decrypt_next_byte(ciphertexts, i) }
+    keystream << candidate_keys[0].to_s
+  end
 
-    keystream
+  keystream
 end
 
 ciphertexts = $plaintexts.map { |enc| MatasanoLib::AES_128.encrypt(enc.decode64, $AES_KEY, :mode => :CTR) }
