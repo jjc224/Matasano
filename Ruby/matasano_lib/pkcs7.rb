e2a327f9d3d3b1d4dcb59c5356b9268e61660e59
jjc224 <youngphizo@gmail.com>
2018-12-29 16:54:14 +1100

    Updated tabs to two spaces.

--- Ruby/matasano_lib/pkcs7.rb
+++ Ruby/matasano_lib/pkcs7.rb
@@ -1,29 +1,29 @@
 module MatasanoLib
-	module PKCS7
-		class << self
-			@@blocksize = 16
+  module PKCS7
+    class << self
+      @@blocksize = 16
 
-			def pad(str, blocksize = @@blocksize)
-				padding = blocksize - (str.length % blocksize)
-				str + padding.chr * padding
-			end
+      def pad(str, blocksize = @@blocksize)
+        padding = blocksize - (str.length % blocksize)
+        str + padding.chr * padding
+      end
 
-			def valid(str)
-				padding   = str[-1]
-				pad_start = str.length - padding.ord
+      def valid(str)
+        padding   = str[-1]
+        pad_start = str.length - padding.ord
 
-				# TODO: str.ends_with?(padding * padding.ord)
-				pad_start < str.length && str[pad_start..-1] == padding * padding.ord
-			end
+        # TODO: str.ends_with?(padding * padding.ord)
+        pad_start < str.length && str[pad_start..-1] == padding * padding.ord
+      end
 
-			def strip(str, blocksize = @@blocksize)
-				padding   = str[-1]
-				pad_start = str.length - padding.ord
+      def strip(str, blocksize = @@blocksize)
+        padding   = str[-1]
+        pad_start = str.length - padding.ord
 
-				# My bad, man, didn't mean to fuck with your string. Here you go. Sorry for any inconvenience.
-				return str unless valid(str)
-				str[0..pad_start - 1]
-			end
-		end
-	end
+        # My bad, man, didn't mean to fuck with your string. Here you go. Sorry for any inconvenience.
+        return str unless valid(str)
+        str[0..pad_start - 1]
+      end
+    end
+  end
 end
