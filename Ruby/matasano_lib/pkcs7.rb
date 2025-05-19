8b9e868cdc2b0593667a544209e63b54ece2083f
jjc224 <14149636+jjc224@users.noreply.github.com>
2025-05-19 17:30:15 +1000

    Touched up to be more readable / less C like.

--- Ruby/matasano_lib/pkcs7.rb
+++ Ruby/matasano_lib/pkcs7.rb
@@ -9,19 +9,21 @@ module MatasanoLib
       end
 
       def valid(str)
-        padding   = str[-1]
-        pad_start = str.length - padding.ord
+        pad_char = str[-1]
+        padding  = pad_char * pad_char.ord
 
-        # TODO: str.ends_with?(padding * padding.ord)
-        pad_start < str.length && str[pad_start..-1] == padding * padding.ord
+        # Confirm the last byte of `str` is within the range of valid PKCS #7 values and `str` ends with a sequence of that value.
+        # (Leans on short-circuit evaluation of left operand.)
+        pad_char.ord.between?(1, @@blocksize) && str.end_with?(padding)
       end
 
       def strip(str, blocksize = @@blocksize)
-        padding   = str[-1]
-        pad_start = str.length - padding.ord
-
         # My bad, man, didn't mean to fuck with your string. Here you go. Sorry for any inconvenience.
         return str unless valid(str)
+
+        pad_char  = str[-1]
+        pad_start = str.length - pad_char.ord
+
         str[0..pad_start - 1]
       end
     end
